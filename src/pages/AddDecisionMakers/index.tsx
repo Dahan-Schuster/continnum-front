4f2b9bcec2e271a76289c312b0403bacdb7a3408
Dahan Schuster <dan.plschuster@gmail.com>
2023-04-10 01:10:46 -0300

    feat: Adiciona rota para a página AddDecisionMakers

---
+++ src/pages/AddDecisionMakers/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface AddDecisionMakersProps {
+
+}
+
+/**
+* AddDecisionMakers documentation
+*/
+const AddDecisionMakers: React.FunctionComponent<AddDecisionMakersProps> = () => {
+	return (
+		<div>
+			yo!
+		</div>
+	);
+};
+
+export default AddDecisionMakers;
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import ReactSlideRoutes from "react-slide-routes";
+import AddDecisionMakers from "../AddDecisionMakers";
 import EntryPage from "./EntryPage";
 
 interface MainPageProps {}
@@ -12,6 +13,11 @@ const MainPage: React.FunctionComponent<MainPageProps> = () => {
   return (
     <ReactSlideRoutes duration={1000}>
       <Route index element={<EntryPage />} />
+      <Route path="/dms" element={<AddDecisionMakers />} />
+      {/*
+			<Route path="/criteria" element={<AddCriteria />} />
+      <Route path="/alternatives" element={<AddAlternatives />} />
+			*/}
     </ReactSlideRoutes>
   );
 };
