b441b19306c84e91a9bb631a4ee9c529cca9b5eb
Dahan Schuster <dan.plschuster@gmail.com>
2023-04-10 01:07:42 -0300

    feat: Estilo padrão das páginas do sistema

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
+import { makeStyles } from "@mui/styles";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { ModelProvider } from "./contexts/ModelContext";
 import MainPage from "./pages/Main";
 
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    background: "#f7f7f7",
+  },
+  mainContainer: {
+    background: "#f1f1f1",
+    borderRadius: 8,
+    border: "1px black solid",
+		padding: "2rem 4rem",
+  },
+});
+
 function App() {
+  const classes = useStyles();
   return (
     <ModelProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<MainPage />} />
-        </Routes>
-      </BrowserRouter>
+      <div className={classes.root}>
+        <div className={classes.mainContainer}>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<MainPage />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
     </ModelProvider>
   );
 }
