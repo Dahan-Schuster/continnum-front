c76d43bd3de014ea81dce1c5ffd429984c6a5401
Dahan Schuster <dan.plschuster@gmail.com>
2023-04-10 00:15:24 -0300

    feat: Componente inicial da página principal
    Adiciona o código inicial da página principal, para poder usá-la na
    definição das rotas do sistema

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import './styles.css';
+
+interface MainPageProps {
+
+}
+
+/**
+* Página inicial do sistema
+*/
+const MainPage: React.FunctionComponent<MainPageProps> = () => {
+	return (
+		<div >
+			yo!
+		</div>
+	);
+};
+
+export default MainPage;
