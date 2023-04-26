d86fce79a3e0927842ea4a2ca3b110c8d1f353e0
Dahan Schuster <dan.plschuster@gmail.com>
2023-04-26 01:50:11 -0300

    feat: Declaração dos objetos de Passo Atual do modelo

--- src/declarations/index.ts
+++ src/declarations/index.ts
@@ -1,23 +1,23 @@
 export interface CriterionJudgment {
-	criterion_id: string  // criterion being judged by the DecisionMaker
-	sp: Number  // "lower" value of the fuzzy judgement
-	sq: Number  // "higher" value of the fuzzy judgement
-	positive_distance?: Number
-	negative_distance?: Number
+  criterion_id: string; // criterion being judged by the DecisionMaker
+  sp: Number; // "lower" value of the fuzzy judgement
+  sq: Number; // "higher" value of the fuzzy judgement
+  positive_distance?: Number;
+  negative_distance?: Number;
 }
 
 export interface DecisionMaker {
-	id: string;
-	criterion_judgments: CriterionJudgment[];
-	positive_ideal_solution: [Number, Number]; // the two values of the positive ideal solution
-	negative_ideal_solution: [Number, Number]; // the two values of the negative ideal solution
-	name: string;
-	weight: number;
+  id: string;
+  criterion_judgments: CriterionJudgment[];
+  positive_ideal_solution: [Number, Number]; // the two values of the positive ideal solution
+  negative_ideal_solution: [Number, Number]; // the two values of the negative ideal solution
+  name: string;
+  weight: number;
 }
 
-export interface Alternative { 
-	id: string;
-	name: string;
+export interface Alternative {
+  id: string;
+  name: string;
   total_distance_positive: Number;
   total_distance_negative: Number;
   score: Number;
@@ -25,35 +25,47 @@ export interface Alternative {
 }
 
 export interface AggregatedJudgment {
-	id: string;
-	alternative_id: string;
-	sp: Number;
-	sq: Number;
-	positive_distance: Number;
-	negative_distance: Number;
+  id: string;
+  alternative_id: string;
+  sp: Number;
+  sq: Number;
+  positive_distance: Number;
+  negative_distance: Number;
 }
 
 export interface AlternativeJudgment {
-	id: string;
-	decision_maker_id: string;
-	max_value: Number;
-	min_value: Number;
+  id: string;
+  decision_maker_id: string;
+  max_value: Number;
+  min_value: Number;
 }
 
 export type CriterionType = "benefit" | "cost";
 
 export interface Criterion {
   id: string;
-	description: string;
-	criterion_type: CriterionType;
-	total_positive_distance: 0  // distance of this criterion to the positive ideal
-  total_negative_distance: 0  // distance of this criterion to the negative ideal
-  cci: 0  // unnormalized weight
-  normalized_cci: 0  // weight (level of importance) of the criterion
-  unnormalized_distance: 0
-  normalized_distance: 0
+  description: string;
+  criterion_type: CriterionType;
+  total_positive_distance: 0; // distance of this criterion to the positive ideal
+  total_negative_distance: 0; // distance of this criterion to the negative ideal
+  cci: 0; // unnormalized weight
+  normalized_cci: 0; // weight (level of importance) of the criterion
+  unnormalized_distance: 0;
+  normalized_distance: 0;
   alternative_judgments: AlternativeJudgment[];
   aggregated_judgments: AggregatedJudgment[];
-  positive_ideal_solution: Number[];  // the two values of the positive ideal solution
-  negative_ideal_solution: Number[];  // the two values of the negative ideal solution
+  positive_ideal_solution: Number[]; // the two values of the positive ideal solution
+  negative_ideal_solution: Number[]; // the two values of the negative ideal solution
 }
+
+export interface ModelProgressStep {
+  order: number;
+  label: string;
+  description: string;
+}
+
+export const ModelSteps: ModelProgressStep[] = [
+  { order: 1, label: "A", description: "Definir Problema" },
+  { order: 2, label: "B", description: "Inserir Julgamentos" },
+  { order: 3, label: "C", description: "Resultados" },
+];
