dfdb121d92e16266c364085bb2193d9445535924
Dahan Schuster <dan.plschuster@gmail.com>
2023-05-03 00:27:54 -0300

    refact: Move lógica de DMs p/ o hook useDecisionMakers

--- src/contexts/ModelContext.tsx
+++ src/contexts/ModelContext.tsx
@@ -1,33 +1,25 @@
-import { v4 as uuidv4 } from "uuid";
-import produce from "immer";
 import React from "react";
 import {
   Alternative,
   Criterion,
-  CriterionJudgment,
-  DecisionMaker,
   ModelProgressStep,
   ModelSteps,
 } from "../declarations";
+import useDecisionMakers, {
+  DecisionMakersHookValues,
+} from "../hooks/useDecisionMakers";
 
 interface ModelContextValue {
   currentStep: ModelProgressStep;
   criteria: Criterion[];
   setCriteria: (x: Criterion[]) => void;
-  decisionMakers: DecisionMaker[];
-  addDecisionMaker: (name: string, weight: number) => void;
-  setDecisionMakers: (dms: DecisionMaker[]) => void;
-  deleteDecisionMaker: (decisionMakerId: string) => void;
-  getTotalDecisionMakersWeight: () => number;
   alternatives: Alternative[];
   setAlternatives: (alts: Alternative[]) => void;
-  addCriterionJudgmentFromDecisionMaker: (
-    decisionMakerId: string,
-    criterionJudgment: CriterionJudgment
-  ) => void;
 }
 
-const ModelContext = React.createContext<ModelContextValue | null>(null);
+const ModelContext = React.createContext<
+  (ModelContextValue & DecisionMakersHookValues) | null
+>(null);
 export default ModelContext;
 
 /**
@@ -42,68 +34,16 @@ export const ModelProvider: React.FC<{ children: React.ReactNode }> = ({
   );
 
   const [criteria, setCriteria] = React.useState<Criterion[]>([]);
-  const [decisionMakers, setDecisionMakers] = React.useState<DecisionMaker[]>(
-    []
-  );
   const [alternatives, setAlternatives] = React.useState<Alternative[]>([]);
 
-  const addDecisionMaker = React.useCallback((name: string, weight: number) => {
-    setDecisionMakers(
-      produce((draft) => {
-        draft.push({
-          id: uuidv4(),
-          name: name,
-          weight: weight,
-          criterion_judgments: [],
-          positive_ideal_solution: [0, 0],
-          negative_ideal_solution: [0, 0],
-        });
-      })
-    );
-  }, []);
-
-  /**
-   * Deleta um DecisionMaker da lista a partir de seu ID
-   */
-  const deleteDecisionMaker = React.useCallback((decisionMakerId: string) => {
-    setDecisionMakers(
-      produce((draft) => {
-        return draft.filter((dm) => dm.id !== decisionMakerId);
-      })
-    );
-  }, []);
-
-  /**
-   * Calcula de retorna o valor total dos pesos dos decisores, arredondado
-   * para o decimal mais próximo
-   */
-  const getTotalDecisionMakersWeight = React.useCallback(() => {
-    let totalWeight = decisionMakers.reduce(
-      (accumulator, currentValue) => accumulator + currentValue.weight,
-      0
-    );
-
-    totalWeight = Math.ceil(totalWeight * 10) / 10;
-    return totalWeight;
-  }, [decisionMakers]);
-
-  const addCriterionJudgmentFromDecisionMaker = React.useCallback(
-    (decisionMakerId: string, { criterion_id, sp, sq }: CriterionJudgment) => {
-      setDecisionMakers(
-        produce((draft) => {
-          const decisionMaker = draft.find((dm) => dm.id === decisionMakerId);
-          if (decisionMaker) {
-            decisionMaker.criterion_judgments.push({
-              criterion_id,
-              sp,
-              sq,
-            });
-          }
-        })
-      );
-    },
-    []
-  );
+  const {
+    decisionMakers,
+    addDecisionMaker,
+    setDecisionMakers,
+    deleteDecisionMaker,
+    getTotalDecisionMakersWeight,
+    addCriterionJudgmentFromDecisionMaker,
+  } = useDecisionMakers();
 
   return (
     <ModelContext.Provider
@@ -111,13 +51,13 @@ export const ModelProvider: React.FC<{ children: React.ReactNode }> = ({
         currentStep,
         criteria,
         setCriteria,
+        alternatives,
+        setAlternatives,
         decisionMakers,
         addDecisionMaker,
         setDecisionMakers,
         deleteDecisionMaker,
         getTotalDecisionMakersWeight,
-        alternatives,
-        setAlternatives,
         addCriterionJudgmentFromDecisionMaker,
       }}
     >
---
+++ src/hooks/useDecisionMakers.ts
@@ -0,0 +1,98 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import produce from "immer";
+import {CriterionJudgment, DecisionMaker} from "../declarations";
+
+export interface DecisionMakersHookValues {
+  decisionMakers: DecisionMaker[];
+  addDecisionMaker: (name: string, weight: number) => void;
+  setDecisionMakers: (dms: DecisionMaker[]) => void;
+  deleteDecisionMaker: (decisionMakerId: string) => void;
+  getTotalDecisionMakersWeight: () => number;
+  addCriterionJudgmentFromDecisionMaker: (
+    decisionMakerId: string,
+    criterionJudgment: CriterionJudgment
+  ) => void;
+}
+
+/**
+* Hook para gerenciar tomadores de decisão do modelo
+*
+* Usado dentro do ModelContext para ser acessado em
+* qualquer componente dentro do contexto
+*/
+const useDecisionMakers = (): DecisionMakersHookValues => {
+
+  const [decisionMakers, setDecisionMakers] = React.useState<DecisionMaker[]>(
+    []
+  );
+  const addDecisionMaker = React.useCallback((name: string, weight: number) => {
+    setDecisionMakers(
+      produce((draft) => {
+        draft.push({
+          id: uuidv4(),
+          name: name,
+          weight: weight,
+          criterion_judgments: [],
+          positive_ideal_solution: [0, 0],
+          negative_ideal_solution: [0, 0],
+        });
+      })
+    );
+  }, []);
+
+  /**
+   * Deleta um DecisionMaker da lista a partir de seu ID
+   */
+  const deleteDecisionMaker = React.useCallback((decisionMakerId: string) => {
+    setDecisionMakers(
+      produce((draft) => {
+        return draft.filter((dm) => dm.id !== decisionMakerId);
+      })
+    );
+  }, []);
+
+  /**
+   * Calcula de retorna o valor total dos pesos dos decisores, arredondado
+   * para o decimal mais próximo
+   */
+  const getTotalDecisionMakersWeight = React.useCallback(() => {
+    let totalWeight = decisionMakers.reduce(
+      (accumulator, currentValue) => accumulator + currentValue.weight,
+      0
+    );
+
+    totalWeight = Math.ceil(totalWeight * 10) / 10;
+    return totalWeight;
+  }, [decisionMakers]);
+
+  const addCriterionJudgmentFromDecisionMaker = React.useCallback(
+    (decisionMakerId: string, { criterion_id, sp, sq }: CriterionJudgment) => {
+      setDecisionMakers(
+        produce((draft) => {
+          const decisionMaker = draft.find((dm) => dm.id === decisionMakerId);
+          if (decisionMaker) {
+            decisionMaker.criterion_judgments.push({
+              criterion_id,
+              sp,
+              sq,
+            });
+          }
+        })
+      );
+    },
+    []
+  );
+
+	return {
+		decisionMakers,
+		addDecisionMaker,
+		deleteDecisionMaker,
+		setDecisionMakers,
+		getTotalDecisionMakersWeight,
+		addCriterionJudgmentFromDecisionMaker,
+	};
+};
+
+export default useDecisionMakers;
+
