845e48f83169ea1053b3715a160416cff15761c8
Dahan Schuster <dan.plschuster@gmail.com>
2023-05-04 10:32:02 -0300

    feat: Hook para gerenciar o passo atual do Modelo
    Este hook guarda as funções e estados referentes ao passo atual do
    modelo, ou seja, em que parte do processo u usuárie se encontra, para
    onde deve ir e a tela anterior

---
+++ src/hooks/useModelSteps.ts
@@ -0,0 +1,77 @@
+import React, { useCallback } from "react";
+
+export interface ModelPorgressSubstep {
+  label: string;
+  description: string;
+  route: string;
+  nextRoute?: string;
+  prevRoute?: string;
+}
+
+export interface ModelProgressStep {
+  order: number;
+  label: string;
+  description: string;
+  substeps: ModelPorgressSubstep[];
+}
+
+export const ModelSteps: ModelProgressStep[] = [
+  {
+    order: 1,
+    label: "A",
+    description: "Definir Problema",
+    substeps: [
+      {
+        label: "passo 1",
+        description: "informe o nome e o peso de cada um dos decisores",
+        route: "/model/dms",
+        nextRoute: "/model/criteria",
+        prevRoute: undefined,
+      },
+      {
+        label: "passo 2",
+        description: "Informe e classifique os critérios",
+        route: "/model/criteria",
+        nextRoute: "/model/alternatives",
+        prevRoute: "/model/dms",
+      },
+    ],
+  },
+  { order: 2, label: "B", description: "Inserir Julgamentos", substeps: [] },
+  { order: 3, label: "C", description: "Resultados", substeps: [] },
+];
+
+export interface ModelStepsHookValues {
+  initialStep: ModelProgressStep;
+  currentStep: ModelProgressStep;
+  setCurrentStep: (step: ModelProgressStep) => void;
+  canGoForward: boolean;
+  setCanGoForward: (doesIt: boolean) => void;
+  canGoBack: boolean;
+  setCanGoBack: (doesIt: boolean) => void;
+}
+
+/**
+ * Hook para buscar os passos do modelo
+ * Define os passos existentes e expõe um estado com o passo atual
+ */
+const useModelSteps = (): ModelStepsHookValues => {
+  const initialStep = React.useRef(ModelSteps[0]).current;
+  const [currentStep, setCurrentStep] =
+    React.useState<ModelProgressStep>(initialStep);
+
+  const [canGoForward, setCanGoForward] = React.useState<boolean>(false);
+  const [canGoBack, setCanGoBack] = React.useState<boolean>(false);
+
+  return {
+    initialStep,
+    currentStep,
+    setCurrentStep,
+    canGoForward,
+    setCanGoForward,
+    canGoBack,
+    setCanGoBack,
+  };
+};
+
+export default useModelSteps;
