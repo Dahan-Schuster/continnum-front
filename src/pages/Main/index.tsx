e767c0d09a91b54d770c0785a322181a9d0958c2
Dahan Schuster <dan.plschuster@gmail.com>
2023-05-04 11:45:35 -0300

    feat: Navegação entre as rotas na MainPage
    Adiciona um rodapé com botões de navegação na MainPage, podendo ser
    habilitados e desabilitados alterando os estados canGoBack e
    canGoForward (importados do ModelContext) em qualquer lugar
    
    Os botões chamam os métodos goForward e goBack exportados pelo
    ModelContext e implementados pelo useModelSteps
    
    Um useEffect escuta por mudanças no estado currentRoute e navega para a
    rota definida na prop currentRoute.name

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,12 @@
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useNavigate } from "react-router-dom";
 import ReactSlideRoutes from "react-slide-routes";
 import Header from "../../components/Header";
 import AddDecisionMakers from "../AddDecisionMakers";
+
+import { useModel } from "../../contexts/ModelContext";
 import EntryPage from "./EntryPage";
 
 interface MainPageProps {}
@@ -13,12 +15,28 @@ interface MainPageProps {}
  * Página inicial do sistema
  */
 const MainPage: React.FunctionComponent<MainPageProps> = () => {
+  // dados do modelo
+  const { goForward, goBack, canGoBack, canGoForward, currentRoute } =
+    useModel();
+
+  // Método para navegar pra a próxima tela
+  const navigate = useNavigate();
+
+  /**
+   * Efeito lançado ao mudar a rota atual no useModelSteps
+   * Navega para a rota usando o navigate
+   */
+  React.useEffect(() => {
+    navigate(currentRoute.name);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentRoute]);
+
   return (
     <Grid container height="100vh">
       <Grid item xs={12} height="fit-content">
         <Header />
       </Grid>
-      <Grid item xs={12} mt={5} height="100%">
+      <Grid item xs={12} mt={5} height="auto">
         <Box
           display={"flex"}
           justifyContent="center"
@@ -35,8 +53,29 @@ const MainPage: React.FunctionComponent<MainPageProps> = () => {
           </ReactSlideRoutes>
         </Box>
       </Grid>
-      <Grid item xs={12} height="fit-content">
-        <div>rodapé</div>
+      <Grid
+        item
+        xs={12}
+        px={5}
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={goBack}
+          disabled={!canGoBack}
+        >
+          Voltar
+        </Button>
+        <Button
+          variant="contained"
+          onClick={goForward}
+          disabled={!canGoForward}
+        >
+          Avançar
+        </Button>
       </Grid>
     </Grid>
   );
