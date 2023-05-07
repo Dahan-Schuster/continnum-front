8e027180b6bcfca8f873b9379e309f1efaadedb6
Dahan Schuster <dan.plschuster@gmail.com>
2023-05-07 19:10:40 -0300

    layout: Cores do ModelProgress

--- src/components/ModelProgress.tsx
+++ src/components/ModelProgress.tsx
@@ -2,7 +2,7 @@ import { Grid, Box, Typography } from "@mui/material";
 import React from "react";
 import { useModel } from "../contexts/ModelContext";
 import { ModelSteps } from "../hooks/useModelSteps";
-import colors from "../utils/colors";
+import {useMuiTheme} from "../hooks/useMuiTheme";
 
 interface ModelProgressProps {}
 
@@ -13,15 +13,17 @@ const stepCircleSize = 25;
  */
 const ModelProgress: React.FunctionComponent<ModelProgressProps> = () => {
   const { currentStep } = useModel();
+	const { palette } = useMuiTheme();
 
   return (
     <Grid maxWidth={"sm"} container mx={"auto"} py={3}>
       {ModelSteps.map((step) => {
+        const isDone = step.order < currentStep.order;
         const isCurrent = step.order === currentStep.order;
 
         return (
           <Grid
-						key={step.order}
+            key={step.order}
             item
             xs={4}
             display={"flex"}
@@ -37,17 +39,24 @@ const ModelProgress: React.FunctionComponent<ModelProgressProps> = () => {
                 width: `${stepCircleSize}px`,
                 height: `${stepCircleSize}px`,
                 borderRadius: `${stepCircleSize / 2}px`,
-                borderColor: isCurrent ? colors.strong : colors.disabled,
-                borderWidth: isCurrent ? 2 : 1,
+                borderColor: isCurrent || isDone ? palette.primary.main : palette.secondary.main,
+                backgroundColor: isDone ? palette.primary.main : "#fff",
+                borderWidth: 2,
                 borderStyle: "solid",
               }}
             >
-              <Typography component={"h2"}>{step.label}</Typography>
+              <Typography
+                component={"h2"}
+                color={isDone ? "primary.contrastText" : (isCurrent ? "primary.main" : "text.disabled")}
+              >
+                {step.label}
+              </Typography>
             </Box>
             <Typography
-              fontSize={"0.9rem"}
+              fontSize={"0.8rem"}
               component={"h3"}
-              fontWeight={isCurrent ? "bold" : "normal"}
+              color={isCurrent || isDone ? "text.secondary" : "text.disabled"}
+							fontWeight={isCurrent ? "bold" : "normal"}
             >
               {step.description}
             </Typography>
