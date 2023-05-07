c748761f35ce09293ef31e0962c19f12edac3cc5
Dahan Schuster <dan.plschuster@gmail.com>
2023-05-07 18:32:48 -0300

    feat: Define tema do App usando o Mui ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
+import { ThemeProvider } from "@emotion/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { ModelProvider } from "./contexts/ModelContext";
+import { useMuiTheme } from "./hooks/useMuiTheme";
 import MainPage from "./pages/Main";
 
 function App() {
+  const muiTheme = useMuiTheme();
+
   return (
-    <ModelProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/model/*" element={<MainPage />} />
-        </Routes>
-      </BrowserRouter>
-    </ModelProvider>
+    <ThemeProvider theme={muiTheme}>
+      <ModelProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/model/*" element={<MainPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ModelProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ src/hooks/useMuiTheme.ts
@@ -0,0 +1,35 @@
+import { createTheme } from "@mui/material";
+
+export const useMuiTheme = () => {
+  const theme = createTheme({
+    typography: {
+      //  "fontFamily": ["Poppins"].join(','),
+      //  "fontSize": 14,
+      //  "fontWeightLight": 300,
+      //  "fontWeightRegular": 400,
+      //  "fontWeightMedium": 500
+    },
+    palette: {
+      // mode: 'dark',
+
+      primary: {
+        main: "#FF715B",
+				light: "#ffeeed",
+				contrastText: "#FFFFFF",
+      },
+      secondary: {
+        main: "#6C757D",
+      },
+      text: {
+				primary: "#212529",
+        disabled: "#6C757D",
+				secondary: "#FF715B",
+      },
+      // error: {
+      //     main: COLORS.red,
+      // }
+    },
+  });
+
+  return theme;
+};
