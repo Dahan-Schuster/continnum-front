65d85e1dc1ed22bbf50d355508e633e3a4cae607
Dahan Schuster <dan.plschuster@gmail.com>
2023-05-11 00:46:58 -0300

    feat: Página para selecionar a escala linguística

--- src/pages/SelectLinguisticScale/index.tsx
+++ src/pages/SelectLinguisticScale/index.tsx
@@ -1,4 +1,14 @@
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import { useModel } from "../../contexts/ModelContext";
+import { ModelLinguisticScales } from "../../hooks/useModelScale";
 
 interface SelectLinguisticScaleProps {}
 
@@ -8,7 +18,63 @@ interface SelectLinguisticScaleProps {}
 const SelectLinguisticScale: React.FunctionComponent<
   SelectLinguisticScaleProps
 > = () => {
-  return <div>{/*TODO: Component body*/}</div>;
+  const { linguisticScale, selectLinguisticScale } = useModel();
+
+  /**
+   * Callback de mudança do RadioGroup
+   * Seleciona a escala linguística com o ID igual ao valor do radio
+   */
+  const handleOnChangeScale = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      selectLinguisticScale(Number(event.target.value));
+    },
+    [selectLinguisticScale]
+  );
+
+  return (
+    <Box display="flex" justifyContent="center">
+      <FormControl>
+        <RadioGroup
+          aria-labelledby="linguistic-scale-radio-buttons-group-label"
+          defaultValue={1}
+          name="linguistic-scale-radio-buttons-group"
+          onChange={handleOnChangeScale}
+        >
+          <FormLabel id="linguistic-scale-radio-buttons-group-label" />
+          {ModelLinguisticScales.map((scale) => {
+            const checked = linguisticScale.id === scale.id;
+            return (
+              <FormControlLabel
+                checked={checked}
+                key={String(scale.id)}
+                value={scale.id}
+                control={
+                  <Radio
+                    sx={{
+                      "& .MuiSvgIcon-root": {
+                        fontSize: 28,
+                      },
+                    }}
+                  />
+                }
+                label={scale.toString()}
+                sx={{
+                  "& .MuiFormControlLabel-label": {
+                    color: checked ? "text.secondary" : "text.disabled",
+                    fontWeight: 400,
+                    padding: "8px 16px",
+                    backgroundColor: checked ? "primary.light" : "transparent",
+                    borderRadius: "6px",
+                    maxWidth: "400px",
+                  },
+                }}
+              />
+            );
+          })}
+        </RadioGroup>
+      </FormControl>
+    </Box>
+  );
 };
 
 export default SelectLinguisticScale;
