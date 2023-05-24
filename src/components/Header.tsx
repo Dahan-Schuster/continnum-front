b1414ca063efe6f4ba3724c5fcb8702aa474f9d4
Dahan Schuster <dan.plschuster@gmail.com>
2023-05-23 21:58:33 -0300

    feat: Nome do app no cabeçalho

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
-import {Grid} from "@mui/material";
+import {Grid, Typography} from "@mui/material";
 import React from "react";
 import ModelProgress from "./ModelProgress";
+
+import config from "../config.json";
 
 interface HeaderProps {}
 
@@ -10,6 +12,15 @@ interface HeaderProps {}
 const Header: React.FunctionComponent<HeaderProps> = () => {
   return (
     <Grid container width={"100%"}>
+			<Grid item xs={12} mb={5}>
+				<Typography
+					color="primary"
+					variant="h1"
+					sx={{ fontSize: '2.5rem' }}
+				>
+				{config.appName}
+				</Typography>
+			</Grid>
       <ModelProgress />
     </Grid>
   );
