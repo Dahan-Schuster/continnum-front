19a74f150e46f6c3f931aa3cbb820c7d1a2da7f5
Dahan Schuster <dan.plschuster@gmail.com>
2023-05-23 22:50:52 -0300

    feat: salvamento do modelo no localStorage

--- src/contexts/ModelContext.tsx
+++ src/contexts/ModelContext.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Alternative } from "../declarations";
-import useAlternatives, {AlternativesHookValues} from "../hooks/useAlternatives";
+import { Model } from "../declarations/model";
+import useAlternatives, {
+  AlternativesHookValues,
+} from "../hooks/useAlternatives";
 import useCriteria, { CriteriaHookValues } from "../hooks/useCriteria";
 import useDecisionMakers, {
   DecisionMakersHookValues,
 } from "../hooks/useDecisionMakers";
-import useModelScale, {ModelScaleHookValues} from "../hooks/useModelScale";
+import useModelScale, { ModelScaleHookValues } from "../hooks/useModelScale";
 
 import useModelSteps, { ModelStepsHookValues } from "../hooks/useModelSteps";
+
+import config from "../config.json";
 
 interface ModelContextValue {
   alternatives: Alternative[];
@@ -20,7 +25,7 @@ const ModelContext = React.createContext<
       CriteriaHookValues &
       AlternativesHookValues &
       ModelStepsHookValues &
-			ModelScaleHookValues)
+      ModelScaleHookValues)
   | null
 >(null);
 export default ModelContext;
@@ -38,14 +43,70 @@ export const ModelProvider: React.FC<{ children: React.ReactNode }> = ({
   const criteriaUtils = useCriteria();
   const alternativesUtils = useAlternatives();
 
+  const [isSafeToSaveToStorage, setIsSafeToSaveToStorage] =
+    React.useState<boolean>(false);
+
+  /**
+   * Retrieving data from the storage at initialization
+   */
+  React.useEffect(() => {
+    try {
+      const storedModel = localStorage.getItem(config.keysStorage.model);
+
+      if (!storedModel) return;
+      let modelState: Model = JSON.parse(storedModel);
+
+      if (!modelState) return;
+      decisionMakersUtils.setDecisionMakers(modelState.decisionMakers);
+      criteriaUtils.setCriteria(modelState.criteria);
+      alternativesUtils.setAlternatives(modelState.alternatives);
+      modelScaleUtils.setLinguisticScale(modelState.linguisticScale);
+      modelStepsUtils.setCurrentRoute(modelState.currentRoute);
+    } catch (e) {
+      console.log("failed to load model:", e);
+    }
+
+    setIsSafeToSaveToStorage(true);
+  }, []);
+
+  /**
+   * Salva os dados relevantes do modelo no storage,
+   * mas apenas se for seguro salvar
+   *
+   * o estado isSafeToSaveToStorage se torna true após
+   * o efeito inicial ter buscado os dados do storage
+   * e preenchido os estados com os valores salvos
+   */
+  React.useEffect(() => {
+    if (!isSafeToSaveToStorage) return;
+
+    const modelState: Model = {
+      decisionMakers: decisionMakersUtils.decisionMakers,
+      criteria: criteriaUtils.criteria,
+      alternatives: alternativesUtils.alternatives,
+      linguisticScale: modelScaleUtils.linguisticScale,
+      currentRoute: modelStepsUtils.currentRoute,
+    };
+
+    const modelJSON = JSON.stringify(modelState);
+    localStorage.setItem(config.keysStorage.model, modelJSON);
+  }, [
+    isSafeToSaveToStorage,
+    decisionMakersUtils.decisionMakers,
+    criteriaUtils.criteria,
+    alternativesUtils.alternatives,
+    modelScaleUtils.linguisticScale,
+    modelStepsUtils.currentRoute,
+  ]);
+
   return (
     <ModelContext.Provider
       value={{
-				...modelScaleUtils,
+        ...modelScaleUtils,
         ...modelStepsUtils,
         ...decisionMakersUtils,
         ...criteriaUtils,
-				...alternativesUtils,
+        ...alternativesUtils,
       }}
     >
       {children}
